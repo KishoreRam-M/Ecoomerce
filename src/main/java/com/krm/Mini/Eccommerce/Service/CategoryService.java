07cfb797cf2e021e3506052ed57e2fdcac767f03
KishoreRam-M <mrkisho65@outlook.com>
2025-04-29 18:05:41 +0530

    feat:category service ready

---
+++ src/main/java/com/krm/Mini/Eccommerce/Service/CategoryService.java
@@ -0,0 +1,45 @@
+package com.krm.Mini.Eccommerce.Service;
+
+import com.krm.Mini.Eccommerce.Model.Category;
+import com.krm.Mini.Eccommerce.Repo.CategoryRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepo categoryRepo;
+
+    public CategoryService(CategoryRepo categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
+
+    public List<Category> getALlCategory() {
+        return categoryRepo.findAll();
+    }
+
+    public List<Category> getAllActiveCategory() {
+        return categoryRepo.findByActiveTrue();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+
+
+    }
+
+    @Transactional
+    public Category createCategory(Category category) throws Exception {
+        if (categoryRepo.existsByName(category.getName())) {
+            throw new Exception("Category name Already Exist");
+        }
+        return categoryRepo.save(category);
+    }
+
+    @Transactional
+    public void deleteCategory(Long id) {
+        categoryRepo.deleteById(id);
+    }
+
+}
